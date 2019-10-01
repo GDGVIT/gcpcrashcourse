be84b65f4172ce77733ef4076a6fc629a39f2915
Samarth Nayyar <samarthnayyar123@gmail.com>
2019-10-02 03:03:59 +0530

    Fixed PWA Install

--- js/index.js
+++ js/index.js
@@ -22,3 +22,10 @@ $(document).ready(function(){
     })
 
 })
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can add to home screen
+  showInstallPromotion();
+});
